7485b66fad111cb8d58edcb5a39ebb9811bf26a0
anubhav9454 <104710170+anubhav9454@users.noreply.github.com>
2023-04-26 21:30:06 +0530

    about_us complete

--- bhumi/pages/about.vue
+++ bhumi/pages/about.vue
@@ -29,11 +29,58 @@
                 <div class="bg-[#FF9900] w-28 h-2 rounded-full"></div>
             </div>
         </div>
-        <div>
-            <div>
-                <div>
-                    <img src="">
+        <div class="px-28 py-10">
+            <div class="flex items-center justify-between ">
+                <div class="">
+                    <img width="449" height="300" src="/image/about_2_new.jpg">
                 </div>
+                <p class="text-3xl w-[42rem] ">
+                    Bhumi Group is specialized in copper mining and have been mining copper for the last many years in Democratic Republic of Congo with about 1500 sq. km of mining lease. We have mining licenses in the DRC where we have been producing 10,000 to 15,000 dry metric tonnes of copper ore per day.
+                </p>
+            </div>
+        </div>
+        <div class=" flex justify-center text-4xl font-bold py-5">
+            <div class="flex flex-col items-center gap-2">
+                <div>Our Mission</div>
+                <div class="bg-[#FF9900] w-28 h-2 rounded-full"></div>
+            </div>
+        </div>
+        <div class="px-28 text-3xl">
+            <p>We thrive to explore, develop, build and operate a mining enterprise that produces essential metals such as copper and cobalt etc. for the global community safely and responsible - sustaining and improving our way of life, protecting our natural surroundings and returning value to those who have a stake in us.</p>
+        </div>
+        <div class=" flex justify-center text-4xl font-bold py-5">
+            <div class="flex flex-col items-center gap-2">
+                <div>Our Values</div>
+                <div class="bg-[#FF9900] w-28 h-2 rounded-full"></div>
+            </div>
+        </div>
+        <div class="px-28 py-10 flex justify-between">
+            <div>
+                <img src="/image/about_3.jpg" />
+            </div>
+            <div class="text-3xl w-[42rem]">
+                <p>What we do and how we do it matters. As we mine and process the minerals that make our world better, our work is guided by these core values.</p>
+                <p><span class="font-bold">Health and Safety</span> <br />we foster a healthy and safe working environment.
+                    <ul class="list-disc">
+                        <li>   We are properly trained and equipped to do our work injury-free.</li>
+                        <li> We constantly look out for on another</li> 
+                        <li>  Systems, practices and policies that promote our safety and health are important, but attitude and behaviour matter the most.</li>
+                    </ul>
+                    <span class="font-bold">Environment.</span> <br/>We are respectful and protective of our natural surroundings.
+                    <ul class="list-disc">
+                        <li>We acknowledge the trust others place in us to protect and use resources wisely.</li>
+                        <li>We rely on specific principles, valid data, the best knowledge and sound systems, processes and practices to inform our decisions and meet our environmental obligations.</li>
+                        <li>We care about the environment; after all, we live here, too.</li>
+                    </ul>
+                    
+                    <span class="font-bold">People.</span> <br/>We attract and develop great talent and together, with prupose, we achieve exceptional outcomes.
+                    <ul class="list-disc">
+                        <li>We treat people with fairness, dignity and respect.</li>
+                        <li>We trust our employees to make good choices and challenge ourselves to greater individual responsibility.</li>
+                        <li>We approach our work responsibilites with purpose and determination, our tasks with a sense of pride and ownership, and our achievements and shortcomings with humility.</li>
+                    </ul>
+
+                </p>
             </div>
         </div>
     </div>
